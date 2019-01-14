1095e078f0eba0dc0372fc0207ea6f11f5b36e01
MrDmitriev <info@investprag.eu>
2019-01-14 16:10:05 +0100

    Исправлено всплывающее окно Ошибки

--- js/data-load.js
+++ js/data-load.js
@@ -6,31 +6,33 @@
     window.renderPins.renderPins(properties);
   };
 
-  var errorHandler = function () {
+  var errorHandler = function (errMessage) {
     var errorTemplate = document.querySelector('#error');
     var errorDiv = errorTemplate.content.querySelector('.error');
     var errorElement = errorDiv.cloneNode(true);
     var main = document.querySelector('main');
+    var errorMessage = errorElement.querySelector('.error__message');
+    errorMessage.textContent = errMessage;
     main.appendChild(errorElement);
   };
 
   window.load = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr.response);
-      } else {
-        onError();
-      }
-    });
     xhr.addEventListener('error', function () {
       onError('Произошла ошибка соединения');
     });
     xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
 
     xhr.timeout = 10000; // 10s
     xhr.open('GET', URL);
